c1426637b4086d6ea4a78fac66f852cc06b1cf91
Michal Milner <Michal Milner>
2015-11-02 22:11:43 +0100

    Nahrání domácího úkolu.

---
+++ app/src/main/java/cz/milner/hotel/MainActivity.java
@@ -0,0 +1,60 @@
+package cz.milner.hotel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks on the reservation button */
+    public void openReservation(View view){
+        Intent intent = new Intent(this,Reservation.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks on the services button */
+    public void openServices(View view){
+        Intent intent = new Intent(this,Services.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks on the about button */
+    public void openAbout(View view){
+        Intent intent = new Intent(this,About.class);
+        startActivity(intent);
+    }
+}
